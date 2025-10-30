528c76152b625065050b66bd2bb07300932a607d
fenikek <fenikeklol@gmail.com>
2025-10-30 22:20:21 +0700

    Google Oauth2 is broken

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from secret import important
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-pngv(p(yuq_t9=y8dc-8wmdq6742y$odq@k71!&)2#3o)_%&x1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost','127.0.0.1']
 
 
 # Application definition
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -134,4 +138,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKEND = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
 ]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = important.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY 
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = important.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET 
