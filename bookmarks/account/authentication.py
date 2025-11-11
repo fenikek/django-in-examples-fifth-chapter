91524876a0605d72c6c34b3d1a1cede1ca9b0fc8
fenikek <fenikeklol@gmail.com>
2025-11-11 19:00:42 +0700

    done

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 class EmailAuthBackend:
     print('yep')
@@ -15,3 +16,9 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+        
+def create_profile(backend, user, *args, **kwargs):
+    '''
+    Creating a profile for google user
+    '''
+    Profile.objects.get_or_create(user=user)
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -135,10 +135,23 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-AUTHENTICATION_BACKEND = [
+AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
     'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details'
 ]
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = important.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY 
