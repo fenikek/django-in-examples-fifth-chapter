e3cf3320f3aa1068a3655bd29538b9a6f4db70e9
fenikek <fenikeklol@gmail.com>
2025-12-11 20:03:32 +0700

    i have created user list and detail by book, following system doesn`t work

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 from secret import important
 
+from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -158,3 +159,7 @@ SOCIAL_AUTH_PIPELINE = [
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = important.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY 
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = important.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET 
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail',args=[u.username])    
+}
